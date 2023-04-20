e33d51d5c9a5e1da3a9cbe80cac66b5f5ef131c9
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-21 06:03:49 +0900

    verbose_name unicode string

--- yt_rank/data_api/admin.py
+++ yt_rank/data_api/admin.py
@@ -1,35 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
-# from .models import ThreadInfo
+from .models import ThreadInfoData
 
-# class AdminThreadInfo(admin.ModelAdmin):
-#     model = ThreadInfo
-#     list_display = (
-#         'server_num',
-#         # 'host_name',
-#         'anydesk_id',
-#         # 'hai_ip_account',
-#         # 'total_logged_in',
-#         'thread_index',
-#         'google_logged_in',
-#         'now_state',
-#         'target_state',
-#
-#
-#         # 'user_agent',
-#
-#         'keyword',
-#         'is_filter',
-#         'target_url',
-#         'enter_type',
-#
-#         'google_id',
-#         'google_password',
-#         'google_email',
-#         'proxy',
-#         'last_connected_timestamp'
-#     )
-#
-# admin.site.register(ThreadInfo,AdminThreadInfo)
+class AdminThreadInfo(admin.ModelAdmin):
+    model = ThreadInfoData
+    list_display = (
+        'server_num',
+        # 'host_name',
+        'anydesk_id',
+        # 'hai_ip_account',
+        # 'total_logged_in',
+        'thread_index',
+        'google_logged_in',
+        'now_state',
+        'target_state',
+
+
+        # 'user_agent',
+
+        'keyword',
+        'is_filter',
+        'target_url',
+        'enter_type',
+
+        'google_id',
+        'google_password',
+        'google_email',
+        'proxy',
+        'last_connected_timestamp'
+    )
+
+admin.site.register(ThreadInfo,AdminThreadInfo)
 
