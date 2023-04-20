7bf2ea628718e931875a4bf698cbb56f2f2a6a68
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-21 04:21:15 +0900

    verbose name update

--- yt_rank/data_api/urls.py
+++ yt_rank/data_api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path('get_thread_info_list_from_client', views.GetThreadInfoListFromClient.as_view(), name='get_thread_info_list_from_client'),
-    re_path('get_thread_info_list_from_controller', views.GetThreadInfoListFromController.as_view(), name='get_thread_info_list_from_controller'),
-    re_path('set_thread_info_from_controller', views.SetThreadInfoFromController.as_view(), name='set_thread_info_from_controller')
+    path('get_thread_info_list_from_client', views.GetThreadInfoListFromClient.as_view(), name='get_thread_info_list_from_client'),
+    path('get_thread_info_list_from_controller', views.GetThreadInfoListFromController.as_view(), name='get_thread_info_list_from_controller'),
+    path('set_thread_info_from_controller', views.SetThreadInfoFromController.as_view(), name='set_thread_info_from_controller')
     # re_path('get_data', views.GetData.as_view(), name='get_data'),
     # re_path('set_proxy', views.SetProxy.as_view(), name='set_proxy'),
     # re_path('get_proxy', views.GetProxy.as_view(), name='get_proxy'),
