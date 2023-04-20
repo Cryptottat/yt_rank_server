eff28cc20ed4cf5cddda95b188099d344241197e
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-21 01:41:14 +0900

    static change

--- yt_rank/yt_rank/settings.py
+++ yt_rank/yt_rank/settings.py
@@ -110,11 +110,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_DIR = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    # STATIC_DIR,
-]
 STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_DIR = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+    # STATIC_DIR,
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
