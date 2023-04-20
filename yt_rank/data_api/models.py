00343558c7ec46cf281886a16a33bef90c90a116
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-21 05:36:00 +0900

    verbose_name unicode string

--- yt_rank/data_api/models.py
+++ yt_rank/data_api/models.py
@@ -1,33 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
 class ThreadInfo(models.Model):
-    host_name = models.CharField(max_length=20, null=False, verbose_name="컴터ID")
-    anydesk_id = models.CharField(max_length=20, null=False, verbose_name="애니데스크")
-    hai_ip_account = models.CharField(max_length=20, null=False, verbose_name="Hai-IP")
-    total_logged_in = models.IntegerField(null=False, default=0, verbose_name="총로긴")
-    thread_index = models.IntegerField(null=False, verbose_name="스레드")
-    server_num = models.IntegerField(null=True, default=None, verbose_name="서버")
-    proxy = models.CharField(max_length=20, null=True, default=None, verbose_name="프록시")
-    google_id = models.CharField(max_length=50, null=True, default=None, verbose_name="ID")
-    google_password = models.CharField(max_length=50, null=True, default=None, verbose_name="PASS")
-    google_email = models.CharField(max_length=50, null=True, default=None, verbose_name="Email")
-    user_agent = models.CharField(max_length=200, null=True, default=None, verbose_name="U-agent")
-    google_logged_in = models.BooleanField(null=False, default=False, verbose_name="Lg")
-    keyword = models.CharField(max_length=100, null=True, default=None, verbose_name="검색어")
-    is_filter = models.BooleanField(null=False, default=False, verbose_name="실시간")
-    target_url = models.CharField(max_length=200, null=True, default=None, verbose_name="타겟 주소")
-    enter_type = models.CharField(max_length=20, null=True, default=None, verbose_name="진입 방법")
-    now_state = models.CharField(max_length=20, null=True, default=None, verbose_name="상태")
-    target_state = models.CharField(max_length=20, null=True, default=None, verbose_name="명령")
-    last_connected_timestamp = models.BigIntegerField(null=True, default=None, verbose_name="마지막 접속")
+    host_name = models.CharField(max_length=20, null=False, verbose_name=u"컴터ID")
+    anydesk_id = models.CharField(max_length=20, null=False, verbose_name=u"애니데스크")
+    hai_ip_account = models.CharField(max_length=20, null=False, verbose_name=u"Hai-IP")
+    total_logged_in = models.IntegerField(null=False, default=0, verbose_name=u"총로긴")
+    thread_index = models.IntegerField(null=False, verbose_name=u"스레드")
+    server_num = models.IntegerField(null=True, default=None, verbose_name=u"서버")
+    proxy = models.CharField(max_length=20, null=True, default=None, verbose_name=u"프록시")
+    google_id = models.CharField(max_length=50, null=True, default=None, verbose_name=u"ID")
+    google_password = models.CharField(max_length=50, null=True, default=None, verbose_name=u"PASS")
+    google_email = models.CharField(max_length=50, null=True, default=None, verbose_name=u"Email")
+    user_agent = models.CharField(max_length=200, null=True, default=None, verbose_name=u"U-agent")
+    google_logged_in = models.BooleanField(null=False, default=False, verbose_name=u"Lg")
+    keyword = models.CharField(max_length=100, null=True, default=None, verbose_name=u"검색어")
+    is_filter = models.BooleanField(null=False, default=False, verbose_name=u"실시간")
+    target_url = models.CharField(max_length=200, null=True, default=None, verbose_name=u"타겟 주소")
+    enter_type = models.CharField(max_length=20, null=True, default=None, verbose_name=u"진입 방법")
+    now_state = models.CharField(max_length=20, null=True, default=None, verbose_name=u"상태")
+    target_state = models.CharField(max_length=20, null=True, default=None, verbose_name=u"명령")
+    last_connected_timestamp = models.BigIntegerField(null=True, default=None, verbose_name=u"마지막 접속")
+
     class Meta:
         db_table = 'thread_data'
-        verbose_name = 'Thread Info'
-
-
-
-
+        verbose_name = u'Thread Info'
 
 #
 # from django.db import migrations
