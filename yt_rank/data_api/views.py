bc0c4b461f9b2223f6c558c8308d07e350ff7f43
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-21 03:57:26 +0900

    views update

--- yt_rank/data_api/views.py
+++ yt_rank/data_api/views.py
@@ -4,15 +4,43 @@ from rest_framework.response import Response
 from .models import ThreadInfo
 import time
 # Create your views here.
-class GetThreadInfoFromLocal(APIView):
-    def post(self, request):
 
+class SetThreadInfoFromController(APIView):
+    def post(self, request):
+        host_name = request.data.get('host_name', None)  #
+        anydesk_id = request.data.get('anydesk_id', None)  #
+        thread_index = request.data.get('thread_index', None)  #
+        keyword = request.data.get('keyword',None)
+        is_filter = request.data.get('is_filter',False)
+        target_url = request.data.get('target_url',None)
+        enter_type = request.data.get('enter_type',None)
+        target_state = request.data.get('target_state',None)
+        thread_infos = ThreadInfo.objects.filter(host_name=host_name, anydesk_id=anydesk_id, thread_index=thread_index)
+        if not thread_infos.exist():
+            return {'msg': False}
+        thread = thread_infos.first()
+        thread.keyword = keyword
+        thread.is_filter = is_filter
+        thread.target_url = target_url
+        thread.enter_type = enter_type
+        thread.target_state = target_state
+        thread.save()
+
+class GetThreadInfoListFromController(APIView):
+    def get(self, request):
+        server_num = request.data.get('server_num',None)
+        if server_num is None:
+            return_data = {'thread_list': list(ThreadInfo.objects.values().all())}
+            return Response(data=return_data)
+        return_data = {'thread_list':list(ThreadInfo.objects.filter(server_num=server_num).values().first())}
+        return Response(data=return_data)
+class GetThreadInfoListFromClient(APIView):
+    def post(self, request):
         host_name = request.data.get('host_name', None)  #
         anydesk_id = request.data.get('anydesk_id', None)  #
         hai_ip_account = request.data.get('hai_ip_account', None)  #
         total_logged_in = request.data.get('total_logged_in', None)  #
         thread_list = request.data.get('thread_list', [])  #
-
         for thread in thread_list:
             if not ThreadInfo.objects.filter(host_name=host_name, anydesk_id=anydesk_id, thread_index=thread['thread_index']).exists():
                 ThreadInfo.objects.create(
