399ec1c6d9a919710feb082e0a87964890ef2481
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-21 05:18:46 +0900

    verbose name update

--- yt_rank/data_api/models.py
+++ yt_rank/data_api/models.py
@@ -29,23 +29,51 @@ class ThreadInfo(models.Model):
 
 
 
-# 버보스 네임 변경시 미적용되는거 해결 용도
-
-from django.contrib.contenttypes.models import ContentType
-from django.utils.functional import Promise
-from django.db.models.signals import post_migrate
 
 
-def update_contenttypes_names(**kwargs):
-    for c in ContentType.objects.all():
-        cl = c.model_class()
-        # Promises classes are from translated, mostly django-internal models. ignore them.
-        if cl and not isinstance(cl._meta.verbose_name, Promise):
-            new_name = cl._meta.verbose_name
-            if c.name != new_name:
-                c.name = new_name
-                c.save()
+from django.db import migrations
+TARGET_APP = 'data_api'    # application label migration is for
+
+class Migration(migrations.Migration):
+
+    def __init__(self, name, app_label):
+        # overriding application operated upon
+        super(Migration, self).__init__(name, TARGET_APP)
+
+    # specify what original migration file it replaces
+    # or leave migration loader confused about unapplied migration
+    replaces = ((TARGET_APP, __module__.rsplit('.', 1)[-1]),)
 
 
-post_migrate.connect(update_contenttypes_names, dispatch_uid="update_contenttypes")
-# 버보스 네임 변경시 미적용되는거 해결 용도 done
+
+
+
+
+
+
+
+
+
+
+
+
+# # 버보스 네임 변경시 미적용되는거 해결 용도
+#
+# from django.contrib.contenttypes.models import ContentType
+# from django.utils.functional import Promise
+# from django.db.models.signals import post_migrate
+#
+#
+# def update_contenttypes_names(**kwargs):
+#     for c in ContentType.objects.all():
+#         cl = c.model_class()
+#         # Promises classes are from translated, mostly django-internal models. ignore them.
+#         if cl and not isinstance(cl._meta.verbose_name, Promise):
+#             new_name = cl._meta.verbose_name
+#             if c.name != new_name:
+#                 c.name = new_name
+#                 c.save()
+#
+#
+# post_migrate.connect(update_contenttypes_names, dispatch_uid="update_contenttypes")
+# # 버보스 네임 변경시 미적용되는거 해결 용도 done
