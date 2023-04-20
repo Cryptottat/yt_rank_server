7007c72603c125d1422e81755f2f18cd079a754a
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-21 03:59:08 +0900

    views update

--- yt_rank/data_api/urls.py
+++ yt_rank/data_api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     re_path('get_thread_info_list_from_client', views.GetThreadInfoListFromClient.as_view(), name='get_thread_info_list_from_client'),
-    re_path('get_thread_info_list_from_controller', views.GetThreadInfoListFromController.as_view(), namge='get_thread_info_list_from_controller'),
-    re_path('set_thread_info_from_controller', views.SetThreadInfoFromController.as_view(), namge='set_thread_info_from_controller')
+    re_path('get_thread_info_list_from_controller', views.GetThreadInfoListFromController.as_view(), name='get_thread_info_list_from_controller'),
+    re_path('set_thread_info_from_controller', views.SetThreadInfoFromController.as_view(), name='set_thread_info_from_controller')
     # re_path('get_data', views.GetData.as_view(), name='get_data'),
     # re_path('set_proxy', views.SetProxy.as_view(), name='set_proxy'),
     # re_path('get_proxy', views.GetProxy.as_view(), name='get_proxy'),
