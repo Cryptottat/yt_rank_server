33869a0005e3e12872719350c45dd98f1a080deb
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-21 11:25:15 +0900

    timestamp update

--- yt_rank/data_api/models.py
+++ yt_rank/data_api/models.py
@@ -15,14 +15,14 @@ class ThreadInfoData(models.Model):
     google_email = models.CharField(max_length=50, null=True, default=None, verbose_name=u"Email")
     user_agent = models.CharField(max_length=200, null=True, default=None, verbose_name=u"U-agent")
     google_logged_in = models.BooleanField(null=False, default=False, verbose_name=u"Lg")
-    rank = models.IntegerField(null=True, default=None,verbose_name=u"순위")
-    keyword = models.CharField(max_length=100, null=True, default=None, verbose_name=u"검색어")
+    rank = models.IntegerField(null=True, default=999, verbose_name=u"순위")
+    keyword = models.CharField(max_length=100, null=True, balnk=True, default=None, verbose_name=u"검색어")
     is_filter = models.BooleanField(null=False, default=False, verbose_name=u"실시간")
-    target_url = models.CharField(max_length=200, null=True, default=None, verbose_name=u"타겟 주소")
-    enter_type = models.CharField(max_length=20, null=True, default=None, verbose_name=u"진입 방법")
-    now_state = models.CharField(max_length=20, null=True, default=None, verbose_name=u"상태")
-    target_state = models.CharField(max_length=20, null=True, default=None, verbose_name=u"명령")
-    last_connected_timestamp = models.BigIntegerField(null=True, default=None, verbose_name=u"마지막 접속")
+    target_url = models.CharField(max_length=200, balnk=True, null=True, default=None, verbose_name=u"타겟 주소")
+    enter_type = models.CharField(max_length=20, balnk=True, null=True, default=None, verbose_name=u"진입 방법")
+    now_state = models.CharField(max_length=20, balnk=True, null=True, default=None, verbose_name=u"상태")
+    target_state = models.CharField(max_length=20, balnk=True, null=True, default=None, verbose_name=u"명령")
+    last_connected_timestamp = models.BigIntegerField(null=True, default=0, verbose_name=u"마지막 접속")
 
     class Meta:
         db_table = 'thread_info_data_table'
