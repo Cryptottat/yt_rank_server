9e380de2fa7361eac2229306ec06afb35a784282
morgan-b-kim <morgan-b-kim@gmail.com>
2023-05-13 00:21:49 +0900

    timestamp update

--- yt_rank/rank/admin.py
+++ yt_rank/rank/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Announcement,PricePerHour
+from .models import Announcement,PricePerHour,Order
 
 class AdminAnnouncement(admin.ModelAdmin):
     model = Announcement
@@ -20,5 +20,18 @@ class AdminPricePerHour(admin.ModelAdmin):
 # Register your models here.
 
 # Register your models here.
+class AdminOrder(admin.ModelAdmin):
+    model = Order
+    list_display = (
+        'username',
+        'target_time',
+        'keyword',
+        'target_url',
+        'charge',
+        'order_time',
+    )
+# Register your models here.
 admin.site.register(Announcement,AdminAnnouncement)
 admin.site.register(PricePerHour,AdminPricePerHour)
+admin.site.register(Order,AdminOrder)
+
--- yt_rank/rank/models.py
+++ yt_rank/rank/models.py
@@ -7,12 +7,12 @@ class Announcement(models.Model):
     create_date = models.DateTimeField()
 
 class Order(models.Model):
-    username = models.CharField(max_length=20)
-    target_time = models.DateTimeField()
-    keyword = models.CharField(max_length=20)
-    target_url = models.CharField(max_length=200)
-    charge = models.IntegerField()
-    order_time = models.DateTimeField()
+    username = models.CharField(max_length=20,verbose_name="아이디")
+    target_time = models.DateTimeField(verbose_name="실행시간")
+    keyword = models.CharField(max_length=20,verbose_name="키워드")
+    target_url = models.CharField(max_length=200,verbose_name="주소")
+    charge = models.IntegerField(verbose_name="몇시간동안")
+    order_time = models.DateTimeField(verbose_name="주문시간")
 
 class PricePerHour(models.Model):
     price_per_hour = models.IntegerField()
