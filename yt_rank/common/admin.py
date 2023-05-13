7060c55110ca819b5d8f80c6da6accab619efd70
morgan-b-kim <morgan-b-kim@gmail.com>
2023-05-13 22:16:36 +0900

    telegram update

--- yt_rank/common/admin.py
+++ yt_rank/common/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import User,TelegramInfo
+# Register your models here.
+
 
 class AdminUser(admin.ModelAdmin):
     model = User
@@ -10,11 +10,10 @@ class AdminUser(admin.ModelAdmin):
         'email',
         'point',
     )
-# Register your models here.
-admin.site.register(User,AdminUser)
 
 class AdminTelegramInfo(admin.ModelAdmin):
     model = TelegramInfo
 
 # Register your models here.
+admin.site.register(User,AdminUser)
 admin.site.register(TelegramInfo,AdminTelegramInfo)
